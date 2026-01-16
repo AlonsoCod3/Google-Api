791df378b1e345334118800795c4bdaa001b32c3
yslaedder17 <yslaedder17@gmail.com>
2026-01-16 19:16:25 +0000

    Bug in name of dict and import request

--- functions/productos.py
+++ functions/productos.py
@@ -29,7 +29,7 @@ def agregarCelda(valor, rango):
     # body = {"values": [[valor]]}
     body = {"values": [[
         uuid.uuid4,
-        valor.name if valor.bame else None,
+        valor.name if valor.name else None,
         valor.type if valor.type else None,
         valor.amount if valor.amount else None
         ]]}
--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from functions import probando, productos
 import os
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+import requests
 
 app = Flask(__name__)
 CORS(app)  # Habilita CORS para todas las rutas
