96636c81b88eaf3cfc3a8f29ee37823eaa7ecde0
yslaedder17 <yslaedder17@gmail.com>
2026-01-27 19:54:13 +0000

    Validate fields to create

--- crud/new/new.py
+++ crud/new/new.py
@@ -1,9 +1,11 @@
 from functions import productos
 from flask import jsonify, request
+from crud.get.by_id import encontrarCelda
+from crud.get.by_id import buscarCelda
 import requests
 import uuid
 
-from functions.productos import (DOCUMENT_ID, sheet_search)
+from functions.productos import (DOCUMENT_ID, sheet_search, sheet_valor)
 
 def newe():
     try:
@@ -12,8 +14,27 @@ def newe():
         print("type of data: ", type(data))
         if not data:
             return jsonify({"Error": "JSON inválido o ausente"}), 400
+
+        if "name" not in data or "type" not in data or "amount" not in data:
+            return jsonify({"Error": "Faltan campos requeridos"}), 400
+
+        print("pase los 3 campos")
+
         if not isinstance(data.get("name"), str):
             return ("El 'name' debe ser una cadena")
+        data["name"] = data.get("name").lower()
+        buscarCelda(data.get("name"), "B")
+        validateProduct = encontrarCelda(sheet_valor["fila"])
+
+        if validateProduct != "#N/A":
+            return jsonify({'Error': "Producto ya creado"}), 500
+        
+        if not isinstance(data.get("type"), str):
+            return ("El 'type' debe ser una cadena")
+        data["type"] = data.get("type").lower()
+        
+        if not data.get("amount").isdigit():
+            return ("El 'amount' debe ser un valor numerico")
 
         pro = agregarCelda(data)
         return pro
@@ -25,9 +46,9 @@ def newe():
 def agregarCelda(valor, rango="A"):
     body = {"values": [[
         str(uuid.uuid4()),
-        valor.get("name") if valor.get("name") else None,
-        valor.get("type") if valor.get("type") else None,
-        valor.get("amount") if valor.get("amount") else None
+        valor.get("name"),
+        valor.get("type"),
+        valor.get("amount")
         ]]}
     print(sheet_search,flush=True)
 
