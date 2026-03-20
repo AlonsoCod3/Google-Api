113cd40d991b8296d7f2900f4227634885c1cc5a
yslaedder17 <yslaedder17@gmail.com>
2026-03-20 21:30:09 +0000

    Correccion de nuevos datos para peticion completa

--- crud/get/get_all.py
+++ crud/get/get_all.py
@@ -14,7 +14,7 @@ def get_all():
 
 # FUNCIONALIDAD DEL SERVICIO
 # Consulta y devuelve todos los nombres de los productos
-def obtenerDataResult(rango="B2:B"):
+def obtenerDataResult(rango="A2:F"):
     values = sheet_search + rango
     result = productos.sheet.values().get(spreadsheetId=DOCUMENT_ID, range=values).execute()
     if not result.get('values'):
@@ -22,5 +22,13 @@ def obtenerDataResult(rango="B2:B"):
     print("RESPUESTA: ",result)
     data_res = []
     for product in result.get('values'):
-        data_res.append(product[0])
+        item = {}
+        item["id"] = product[0]
+        item["code"] = product[1]
+        item["name"] = product[2]
+        item["description"] = product[3]
+        item["price"] = product[4]
+        item["variants"] = product[5]
+        data_res.append(item)
+
     return jsonify(data_res)
